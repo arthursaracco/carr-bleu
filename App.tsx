e2c445bb5a61700a1c4aa98136cf3a4932999658
arthursaracco <asaracco@ensc.fr>
2022-02-04 18:12:34 +0100

    button

--- App.tsx
+++ App.tsx
@@ -1,36 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,ScrollView,FlatList } from 'react-native';
-interface person{
-  id:number,
-  firstName:string
-}
+import { StyleSheet, Text, View,ScrollView,FlatList, TouchableOpacity } from 'react-native'
 
-const DATA = [
-  {id:0,firstName:"Benoît"},
-  {id:1,firstName:"Jean-Marc"},
-  {id:2,firstName:"Jérôme"},
-  {id:0,firstName:"Benoît"},
-  {id:1,firstName:"Jean-Marc"},
-  {id:2,firstName:"Jérôme"},
-  {id:0,firstName:"Benoît"},
-  {id:1,firstName:"Jean-Marc"},
-  {id:2,firstName:"Jérôme"},
-]
 export default function App() {
-  const renderItem = ({item}) =>(
-    <View style={styles.containerName}>
-    <Text>{item.firstName}</Text>
-    </View>
-  )
-  return (
+  return(
     <View style={styles.container}>
-    <FlatList
-    data={DATA}
-    renderItem={renderItem}
-    keyExtractor={(item)=>item.id.toString()}
-    />
-
-     </View>
+      <TouchableOpacity
+      style ={styles.button}
+      onPress={() => alert('Good job !')}
+      >
+        <Text style={styles.font}>Allez les gigis</Text>
+      </TouchableOpacity>
+    </View>
   );
 }
 
@@ -60,12 +40,12 @@ containerName:{
   alignItems:'center',
   height:80,
   marginVertical:4,
-}
-
-  
-
-
-  
-
-
+},
+button:{
+  backgroundColor: 'steelblue',
+    height: 300,
+    width: 400,
+    margin:10,
+    marginLeft:0,
+},
 });
