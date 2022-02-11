46f7f3f9ac48d6cea1545fbd56d008e438f0ba58
arthursaracco <asaracco@ensc.fr>
2022-02-11 11:08:22 +0100

    Text input

--- App.tsx
+++ App.tsx
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Alert, StyleSheet, Text, TextInput, View } from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={styles.input}
+        placeholder="Saisissez votre prénom"
+        onSubmitEditing={(event) =>
+          alert(`Bonjour, ${event.nativeEvent.text} `)
+        }
+      ></TextInput>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
+    flexDirection: 'column',
+    justifyContent: 'center',
     alignItems: 'center',
+  },
+  input: {
+    height: 80,
+    fontSize: 25,
     justifyContent: 'center',
   },
-});
+  font: { alignSelf: 'center', fontSize: 20, fontWeight: 'bold' },
+})
